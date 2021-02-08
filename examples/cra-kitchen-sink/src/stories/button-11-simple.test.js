5259806c4f4e6c172687b7532dcd891ccd58874d
Jonathan del Strother <jdelStrother@gmail.com>
2021-02-08 09:51:08 +0000

    Add a simple-as-possible version of storyshots renderer for comparison

---
+++ examples/cra-kitchen-sink/src/stories/button-11-simple.test.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import * as button from './button-11.stories';
+
+Object.entries(button).forEach(([name, Story]) => {
+  if (typeof Story !== 'function') return;
+
+  test(`button ${name} matches snapshot`, () => {
+    const result = renderer.create(<Story />);
+    expect(result.toJSON()).toMatchSnapshot();
+  });
+});
